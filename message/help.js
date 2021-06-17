597bba80cbcea2d66da88dae161c4c310ac7bd03
Tiago Alves <paulocbf0@gmail.com>
2021-06-17 05:06:12 -0300

    First commit

---
+++ message/help.js
@@ -0,0 +1,127 @@
+exports.menu = (prefix, i) => {
+    return `*Xinz Bot*
+Created by Tiago Team
+
+*INFO*
+${i.free} cekprefix
+${i.free} ${prefix}limit
+${i.free} ${prefix}speed
+${i.free} ${prefix}owner
+
+*Coverter / Tools*
+${i.limit} ${prefix}sticker
+${i.limit} ${prefix}toimg
+${i.limit} ${prefix}attp text
+${i.limit} ${prefix}tinyurl url
+${i.limit} ${prefix}nuliskiri text
+${i.limit} ${prefix}translate idioma do código [mensagem / texto de resposta]
+
+*Downloader*
+${i.limit} ${prefix}ytmp4 url
+${i.limit} ${prefix}ytmp3 url
+${i.limit} ${prefix}igdl url
+${i.limit} ${prefix}fbdl url
+${i.limit} ${prefix}tiktok url
+${i.limit} ${prefix}yts query
+${i.limit} ${prefix}play query
+${i.limit} ${prefix}playmp4 query
+
+*Stalker*
+${i.limit} ${prefix}igstalk username
+${i.limit} ${prefix}ghstalk username
+
+*Text Marker*
+${i.limit} ${prefix}blackpink text
+${i.limit} ${prefix}tahta text
+${i.limit} ${prefix}neon text
+${i.limit} ${prefix}glitch text1|text2
+${i.limit} ${prefix}thundername text
+${i.limit} ${prefix}pornhub text1|text2
+
+*Premium*
+${i.owner} ${prefix}addprem @tag
+${i.owner} ${prefix}delprem @tag
+${i.owner} ${prefix}cekprem
+${i.free} ${prefix}listprem
+
+*Ban*
+${i.owner} ${prefix}ban @tag
+${i.owner} ${prefix}unban @tag
+${i.free} ${prefix}listban
+
+*Game*
+${i.free} ${prefix}topbalance
+${i.free} ${prefix}buylimit
+${i.free} ${prefix}buyglimit
+${i.glimit} ${prefix}tictactoe @tag
+${i.glimit} ${prefix}tebakgambar
+${i.glimit} ${prefix}family100
+
+*VVIBU*
+${i.limit} ${prefix}waifu
+${i.limit} ${prefix}loli
+${i.limit} ${prefix}nekonime
+${i.limit} ${prefix}megumin
+${i.limit} ${prefix}sagiri
+${i.limit} ${prefix}shinobu
+
+*Random*
+${i.free} ${prefix}apakah
+${i.free} ${prefix}bisakah
+${i.free} ${prefix}kapankah
+${i.free} ${prefix}hobby
+${i.free} ${prefix}rate
+${i.free} ${prefix}cekbapak
+${i.free} ${prefix}seberapagay
+${i.limit} ${prefix}truth
+${i.limit} ${prefix}dare
+
+*Group*
+${i.free} ${prefix}afk
+${i.free} ${prefix}infogrup
+${i.free} ${prefix}chatinfo
+${i.owner} ${prefix}add 628xx
+${i.owner} ${prefix}kick @tag
+${i.owner} ${prefix}promote @tag
+${i.owner} ${prefix}demote @tag
+${i.free} ${prefix}linkgc
+${i.owner} ${prefix}leave
+${i.owner} ${prefix}setdesc
+${i.owner} ${prefix}setgrupname
+${i.owner} ${prefix}setppgrup
+${i.owner} ${prefix}opengrup
+${i.owner} ${prefix}closegrup
+${i.owner} ${prefix}join
+${i.owner} ${prefix}tagall
+${i.owner} ${prefix}mute
+${i.owner} ${prefix}unmute
+
+*Enable / Disable*
+${i.owner} ${prefix}antilink
+${i.owner} ${prefix}welcome
+${i.owner} ${prefix}left
+${i.owner} ${prefix}antibadword
+${i.free} ${prefix}listbadword
+${i.owner} ${prefix}addbadword
+${i.owner} ${prefix}delbadword
+
+*Owner*
+${i.owner} >
+${i.owner} $
+${i.owner} ${prefix}self
+${i.owner} ${prefix}public
+${i.owner} ${prefix}setpp
+${i.owner} ${prefix}setname
+${i.owner} ${prefix}setbio
+${i.owner} ${prefix}setprefix
+${i.owner} ${prefix}bc
+${i.owner} ${prefix}clearall
+${i.owner} ${prefix}exif nama|author
+
+Note :
+${i.owner} : Recursos de administrador ou proprietário do grupo
+${i.free} : Recursos gratuitos que não usam limites
+${i.limit} : Recursos que usam 1 limite diário
+${i.glimit} : Recursos que usam o limite de 1 jogo
+${i.prem} : Recursos especiais do usuário premium
+}
